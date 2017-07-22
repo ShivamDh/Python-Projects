1a7c58ca59e4b66046d42fd713c46d14ebb26e7f
ShivamDh <shdh8888@yahoo.ca>
2017-07-21 21:12:10 -0400

    Created homepage

--- starter_blog/blogs/models.py
+++ starter_blog/blogs/models.py
@@ -3,10 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Blog(models.Model):
-	title = models.CharField(max_length=150)
+	title = models.CharField(max_length=150, default="New Blog")
 	date = models.DateField()
 	body = models.TextField()
 	summary = models.TextField()
+	imgSrc = models.CharField(max_length=100, default="")
 	
 	def __str__(self):
 		return self.title
--- starter_blog/homepage/urls.py
+++ starter_blog/homepage/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from . import views
+from django.views.generic import ListView
 from blogs.models import Blog
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$',ListView.as_view(queryset=Blog.objects.all().order_by('-date'), 
+		template_name='home.html'))
 ]
