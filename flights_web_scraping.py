98f663fb78b58fa2276343b40acc7d89b0dd1f1b
Shivam Dharme <shdh8888@yahoo.ca>
2017-10-16 23:46:59 -0400

    Get results from extended API from Expedia

--- flights_web_scraping.py
+++ flights_web_scraping.py
@@ -47,7 +47,41 @@ expedia_soup = soup(expedia_response.text, 'html.parser')
 continued_results_divs = expedia_soup.find_all('div', {'id': 'originalContinuationId'})
 
 if len(continued_results_divs) > 0:
-	pass
+	expedia_results_id = continued_results_divs[0].text.strip()
+	expedia_results_url = 'https://www.expedia.ca/Flight-Search-Paging'
+	expedia_results_url += '?c=' + continued_results_id
+	expedia_results_url += '&is=1&sp=asc&cz=200&cn=0'
+	
+	expedia_results_response = get(expedia_more_results_url)
+	expedia_json = json.loads(expedia_results_response.text)
+	
+	flights = expedia_json['content']['legs']
+	flight_keys = list(flights)
+	
+	# send another Expedia request to gather second leg journeys
+	if flight_type == 2:
+		expedia_results_2_url = expedia_results_url.replace(
+			'is=1',
+			'is=0&fl0=' + flight_keys[0]
+		)
+		
+		expedia_results_2_url = expedia_results_2_url.replace('ul=0', 'ul=1')
+		
+		expedia_results_2_response = get(expedia_results_2_url)
+		expedia_2_json = json.loads(expedia_results_2_response.text)
+		
+		flights_2 = expedia_2_json['content']['legs']
+		flight_2_keys = list(flights_2)
+	
+	for flight in flight_keys:
+		start_time = flight['departureTime']['time']
+		end_time = flight['arrivalTime']['time']
+		
+		duration = str(flight['duration']['hours']) + 'h ' + str(flight['duration']['minutes']) + 'm'
+		price = flight['price']['formattedPrice']
+		
+		f.write('Expedia,' + start_time + ',' + end_time + ',' + duration + ',' + price + '\n')
+	
 else:
 	flights = expedia_soup.find_all('li', {'class': 'flight-module'})
 	
