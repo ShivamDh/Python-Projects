760bc2d20312a82f9ddd06c92e04247332b1ec16
Shivam Dharme <shdh8888@yahoo.ca>
2018-04-22 23:10:34 -0400

    Create more routes

--- classified_advertiser/app.py
+++ classified_advertiser/app.py
@@ -6,6 +6,18 @@ app = Flask(__name__, static_url_path='/static')
 def index():
 	return render_template('home.html')
 
+@app.route('/listings')
+def about():
+	return render_template('listings.html')
+
+@app.route('/signin')
+def signin():
+	return render_template('signin.html')
+
+@app.route('/register')
+def register():
+	return render_template('register.html')
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
