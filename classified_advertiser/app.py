1cf7f5ba11d6a49181efc5d432b9c63d3d4adede
Shivam Dharme <shdh8888@yahoo.ca>
2018-04-28 13:31:23 -0400

    Create sample data, list it on listings page

--- classified_advertiser/app.py
+++ classified_advertiser/app.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template
+from data import Listings
 
 app = Flask(__name__, static_url_path='/static')
 
+Listings = Listings()
+
 @app.route('/')
 def index():
-	return render_template('home.html')
+	return render_template('home.html', listings = Listings[:3])
 
 @app.route('/listings')
 def about():
-	return render_template('listings.html')
+	return render_template('listings.html', listings = Listings)
 
 @app.route('/signin')
 def signin():
---
+++ classified_advertiser/data.py
@@ -0,0 +1,45 @@
+def Listings():
+	listings = [
+		{
+			'id': 1,
+			'authod_id': 1,
+			'create_date': '25-05-2017',
+			'title': 'First Listing',
+			'body': 'Selling my first item on classified website',
+			'price': '199.98' 
+		},
+		{
+			'id': 2,
+			'authod_id': 1,
+			'create_date': '27-05-2017',
+			'title': 'Second Listing',
+			'body': 'Selling a second item on classified website',
+			'price': '199.98' 
+		},
+		{
+			'id': 3,
+			'authod_id': 2,
+			'create_date': '01-06-2017',
+			'title': 'Third Listing',
+			'body': 'Selling my first item on classified website, as the 2nd user',
+			'price': '199.98' 
+		},
+		{
+			'id': 4,
+			'authod_id': 3,
+			'create_date': '05-06-2017',
+			'title': 'Fourth Listing',
+			'body': 'Selling my first item on classified website, as the 3rd user',
+			'price': '199.98' 
+		},
+		{
+			'id': 5,
+			'authod_id': 1,
+			'create_date': '07-06-2017',
+			'title': 'Fifth Listing',
+			'body': 'Selling my third item on classified website',
+			'price': '199.98' 
+		}
+	]
+
+	return listings
