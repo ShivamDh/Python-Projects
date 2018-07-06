1e6f84649493fd47bd367717ed1b0f09791a2eec
Shivam Dharme <shdh8888@yahoo.ca>
2018-07-05 23:14:51 -0400

    Connect to local MySQL database and complete registration backend connection

--- classified_advertiser/app.py
+++ classified_advertiser/app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, flash, request
+from flask import Flask, render_template, flash, redirect, url_for, request
 from data import Listings
 from wtforms import Form, StringField, PasswordField, validators
+from passlib.hash import sha256_crypt
+from app_details import db_password, app_secret_key
+import mysql.connector
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -24,15 +27,15 @@ def signin():
 
 class RegisterForm(Form):
 	name = StringField('Name', [validators.Length(min=1, max=100)])
-	username = StringField('Username', [validators.Length(min=3, max=30)])
 	email = StringField('Email', [
 		validators.Length(min=6, max=50),
 		validators.Email(message='Needs to be a valid email')
 	])
+	username = StringField('Username', [validators.Length(min=3, max=30)])
 	password = PasswordField('Password', [
 		validators.Length(min=6, max=50),
 		validators.Regexp(
-			'^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]).{6}$',
+			'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{6,})',
 			message='Need 1 uppercase, 1 lowercase, 1 special digit, 1 number')
 	])
 	confirm = PasswordField('Confirm Password', [
@@ -43,10 +46,27 @@ class RegisterForm(Form):
 def register():
 	form = RegisterForm(request.form)
 	if request.method == 'POST' and form.validate():
-		return render_template('register.html')
+		name = form.name.data
+		email = form.email.data
+		username = form.username.data
+		password = sha256_crypt.encrypt(str(form.password.data))
+
+		cnx = mysql.connector.connect(user='main', password=db_password, host='127.0.0.1', database='classified_advertiser')
+		cursor = cnx.cursor()
+
+		cursor.execute('INSERT INTO users(name, email, username, password) VALUES (%s, %s, %s, %s)', (name, email, username, password))
+
+		cnx.commit()
+
+		cursor.close()
+		cnx.close()
+
+		flash('The user has been registered, start making your listings now!', 'success')
+
+		return redirect(url_for('index'))
 	return render_template('register.html', form=form)
 
 
-
 if __name__ == '__main__':
+	app.secret_key = app_secret_key
 	app.run(debug=True)
